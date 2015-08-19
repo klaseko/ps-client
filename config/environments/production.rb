b552944ec2926f0cb62805f12ac12acbf1089233
Gerard V. Cruz <gerardcruz@live.com>
2015-08-19 09:02:31 +0800

    show exceptions environment variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,5 +81,5 @@ Rails.application.configure do
   config.client_secret = ENV['PS_CLIENT_SECRET']
   config.ps_url = ENV['PS_URL']
 
-  config.action_dispatch.show_exceptions = true
+  config.action_dispatch.show_exceptions = ENV['SHOW_EXEPTIONS']
 end
